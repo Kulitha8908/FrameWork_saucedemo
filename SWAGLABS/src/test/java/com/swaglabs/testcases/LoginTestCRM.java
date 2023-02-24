74dac2871b4df14ab65b62f2c6d5dcc1a29487f8
Kulitha <kulitha8908@gmail.com>
2023-02-24 23:52:32 +0400

    Add before and after class

--- SWAGLABS/src/test/java/com/swaglabs/testcases/LoginTestCRM.java
+++ SWAGLABS/src/test/java/com/swaglabs/testcases/LoginTestCRM.java
@@ -2,6 +2,8 @@ package com.swaglabs.testcases;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import com.swaglabs.pages.LoginPage;
@@ -9,17 +11,30 @@ import com.swaglabs.utility.BrowserFactory;
 
 public class LoginTestCRM {
 	
-WebDriver driver;	
+WebDriver driver;
+
+@BeforeClass
+public void setUp()
+{
+	driver = BrowserFactory.StartApplication(driver,"Chrome", "https://www.saucedemo.com/");
+	
+}
+
 @Test	
 public void loginToApplication()
 {
-	driver = BrowserFactory.StartApplication(driver,"Chrome", "https://www.saucedemo.com/");
+	
 	
 	LoginPage loginpage= PageFactory.initElements(driver, LoginPage.class);
 	loginpage.loginToApp("standard_user", "secret_sauce");
 	
-	BrowserFactory.quitBrowser(driver);
-	}
 	
+	}
+
+@AfterClass
+public void tearDown()
+{
+	BrowserFactory.quitBrowser(driver);
+}
 	
 }
